5c79f6fc184561f2acc9d6db1ef02ccaa20809ac
ishuy <small.part3@gmail.com>
2023-10-11 19:31:05 +0900

    「ゼロ埋め」の定数を追加

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const resetButton = document.querySelector("#reset");
 
 const displayTwoDigitNumber = 2;
 const displayThreeDigitNumber = 3;
+const addZeroToTheLeft = "0";
 
 let startTime;
 let holdTime = 0;
@@ -12,10 +13,10 @@ let timeoutID;
 
 function displayTime() {
   const currentTime = new Date(Date.now() - startTime + holdTime);
-  const h = String(currentTime.getUTCHours()).padStart(displayTwoDigitNumber, "0");
-  const m = String(currentTime.getUTCMinutes()).padStart(displayTwoDigitNumber, "0");
-  const s = String(currentTime.getUTCSeconds()).padStart(displayTwoDigitNumber, "0");
-  const ms = String(currentTime.getUTCMilliseconds()).padStart(displayThreeDigitNumber, "0");
+  const h = String(currentTime.getUTCHours()).padStart(displayTwoDigitNumber, addZeroToTheLeft);
+  const m = String(currentTime.getUTCMinutes()).padStart(displayTwoDigitNumber, addZeroToTheLeft);
+  const s = String(currentTime.getUTCSeconds()).padStart(displayTwoDigitNumber, addZeroToTheLeft);
+  const ms = String(currentTime.getUTCMilliseconds()).padStart(displayThreeDigitNumber, addZeroToTheLeft);
 
   time.textContent = `${h}:${m}:${s}.${ms}`;
   timeoutID = setTimeout(displayTime, 1);
